d060dd130ed55402b73fad291c862b46d163cdd4
plavelo <plastic.velocity@gmail.com>
2021-03-05 23:05:48 +0900

    added callbacks for composables

--- app/src/main/java/org/plavelo/countdown/ui/Indicator.kt
+++ app/src/main/java/org/plavelo/countdown/ui/Indicator.kt
@@ -15,11 +15,6 @@
  */
 package org.plavelo.countdown.ui
 
-import androidx.compose.animation.core.CubicBezierEasing
-import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.keyframes
-import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Column
@@ -31,7 +26,6 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.ProgressIndicatorDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -39,114 +33,56 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import kotlin.math.PI
-import kotlin.math.cos
-import kotlin.math.sin
 
 @Preview
 @Composable
-fun Indicator() {
+fun Indicator(angle: Float = 0f) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
             .wrapContentSize(Alignment.Center)
-            .clip(CircleShape)
+            .clip(CircleShape),
     ) {
-        CircularProgressIndicator(
+        CircularIndicator(
+            angle = angle,
+            size = 320.dp,
             strokeWidth = 48.dp,
         )
     }
 }
 
-// Each rotation is 1 and 1/3 seconds, but 1332ms divides more evenly
-private const val RotationDuration = 3000
-
-// How far the head and tail should jump forward during one rotation past the base point
-private const val JumpRotationAngle = 290f
-
-// The head animates for the first half of a rotation, then is static for the second half
-// The tail is static for the first half and then animates for the second half
-private const val HeadAndTailAnimationDuration = (RotationDuration * 0.5).toInt()
-private const val HeadAndTailDelayDuration = HeadAndTailAnimationDuration
-
-// The easing for the head and tail jump
-private val CircularEasing = CubicBezierEasing(0.4f, 0f, 0.2f, 1f)
-
-// CircularProgressIndicator Material specs
-// Diameter of the indicator circle
-private val IndicatorDiameter = 320.dp
-
 @Composable
-fun CircularProgressIndicator(
+private fun CircularIndicator(
+    angle: Float,
+    size: Dp,
     modifier: Modifier = Modifier,
     color: Color = MaterialTheme.colors.primary,
-    strokeWidth: Dp = ProgressIndicatorDefaults.StrokeWidth
+    strokeWidth: Dp = ProgressIndicatorDefaults.StrokeWidth,
 ) {
     val stroke = with(LocalDensity.current) {
-        Stroke(width = strokeWidth.toPx(), cap = StrokeCap.Square)
+        Stroke(width = strokeWidth.toPx(), cap = StrokeCap.Round)
     }
-    val transition = rememberInfiniteTransition()
-    val endAngle by transition.animateFloat(
-        0f,
-        JumpRotationAngle,
-        infiniteRepeatable(
-            animation = keyframes {
-                durationMillis = HeadAndTailAnimationDuration + HeadAndTailDelayDuration
-                0f at 0 with CircularEasing
-                JumpRotationAngle at HeadAndTailAnimationDuration
-            }
-        )
-    )
     Canvas(
         modifier
             .progressSemantics()
-            .size(IndicatorDiameter)
-            .focusable()
+            .size(size)
+            .focusable(),
     ) {
-        drawIndeterminateCircularIndicator(-90f, endAngle, color, stroke)
+        val radius = stroke.width / 2
+        val dimen = this.size.width - stroke.width
+        drawArc(
+            color = color,
+            startAngle = -90f,
+            sweepAngle = angle,
+            useCenter = false,
+            topLeft = Offset(radius, radius),
+            size = Size(dimen, dimen),
+            style = stroke,
+        )
     }
-}
-
-private fun DrawScope.drawIndeterminateCircularIndicator(
-    startAngle: Float,
-    sweep: Float,
-    color: Color,
-    stroke: Stroke
-) {
-    val radius = size.width / 2
-    val angleOffset = (180.0 / PI).toFloat() * (stroke.width / radius) / 2f
-    val adjustedStartAngle = startAngle + angleOffset
-    val endRadius = stroke.width / 2 * 0.95f
-    val arcDimen = size.width - 2 * endRadius
-    drawArc(
-        color = color,
-        startAngle = adjustedStartAngle,
-        sweepAngle = sweep,
-        useCenter = false,
-        topLeft = Offset(endRadius, endRadius),
-        size = Size(arcDimen, arcDimen),
-        style = stroke
-    )
-    drawCircle(
-        color = color,
-        radius = endRadius,
-        center = Offset(
-            x = cos(PI / 180.0 * (adjustedStartAngle + sweep + angleOffset)).toFloat() * (radius - endRadius) + radius,
-            y = sin(PI / 180.0 * (adjustedStartAngle + sweep + angleOffset)).toFloat() * (radius - endRadius) + radius
-        )
-    )
-    drawCircle(
-        color = color,
-        radius = endRadius,
-        center = Offset(
-            x = cos(PI / 180.0 * startAngle).toFloat() * (radius - endRadius) + radius,
-            y = sin(PI / 180.0 * startAngle).toFloat() * (radius - endRadius) + radius
-        )
-    )
 }
